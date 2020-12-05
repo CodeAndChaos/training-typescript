e526c53d13a3a8d97830c3ca73c70b2823565158
Neal <info@nealbuerger.com>
2020-12-05 19:43:27 +0100

    refactor day 5

--- src/AdventOfCode/2020/05-BinaryBoarding/05-BinaryBoarding.test.ts
+++ src/AdventOfCode/2020/05-BinaryBoarding/05-BinaryBoarding.test.ts
@@ -1,14 +1,6 @@
 import { resolve } from "path"
 import { readDataArrayFromFile } from "../utils/readFiles"
-import { processBinarySymbol, processBoardingString, processColumn, processRow } from "./05-BinaryBoarding"
-
-it ("works for F", () => {
-    expect(processBinarySymbol(0, 127, "0")).toEqual({min: 0, max: 63 })
-})
-
-it( "works for B", () => {
-    expect(processBinarySymbol(0, 63, "1")).toEqual({min: 32, max: 63 })
-})
+import { processBoardingString, processColumn, processRow } from "./05-BinaryBoarding"
 
 it("works for processing a string", () => {
     expect(processRow("FBFBBFF")).toEqual(44)
@@ -19,9 +11,9 @@ it("works for processing a column", () => {
 })
 
 it("processes example strings", () => {
-    expect(processBoardingString("BFFFBBFRRR")).toEqual({row: 70, column: 7, id: 567})
-    expect(processBoardingString("FFFBBBFRRR")).toEqual({row: 14, column: 7, id: 119})
-    expect(processBoardingString("BBFFBBFRLL")).toEqual({row: 102, column: 4, id: 820})
+    expect(processBoardingString("BFFFBBFRRR")).toEqual({ row: 70, column: 7, id: 567 })
+    expect(processBoardingString("FFFBBBFRRR")).toEqual({ row: 14, column: 7, id: 119 })
+    expect(processBoardingString("BBFFBBFRLL")).toEqual({ row: 102, column: 4, id: 820 })
 })
 
 it("finds an answer", async () => {
@@ -34,11 +26,11 @@ it("finds an answer", async () => {
 it("finds an answer for part 2", async () => {
     const inputFilePath = resolve(__dirname, "puzzleinput.txt")
     let inputArr = await readDataArrayFromFile(inputFilePath)
-    let solution = inputArr.map(processBoardingString).map(val => val.id).sort((a, b) => {return a-b})
+    let solution = inputArr.map(processBoardingString).map(val => val.id).sort((a, b) => { return a - b })
     let seat = 0;
-    for (let i = 0; i<solution.length-1; i++){
-        if (solution[i] +1 !== solution[i+1]){
-            seat = solution[i] +1
+    for (let i = 0; i < solution.length - 1; i++) {
+        if (solution[i] + 1 !== solution[i + 1]) {
+            seat = solution[i] + 1
             break;
         }
     }
