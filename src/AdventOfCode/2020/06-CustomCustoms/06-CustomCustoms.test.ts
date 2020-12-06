5a7d98ee417cb9b6b06212ab08e0a7df77e36d2c
Neal <info@nealbuerger.com>
2020-12-06 13:22:48 +0100

    Advent Day 6: dirty solution

---
+++ src/AdventOfCode/2020/06-CustomCustoms/06-CustomCustoms.test.ts
@@ -0,0 +1,33 @@
+import { read } from "fs";
+import { resolve } from "path";
+import { readDataArrayFromFile } from "../utils/readFiles";
+import { processAnswerArray,processAnswerArray2 } from "./06-CustomCustoms";
+
+
+const readData = async (filename: string) => {
+    const inputFilePath = resolve(__dirname, filename)
+    let inputArr = await readDataArrayFromFile(inputFilePath)
+    return inputArr;
+}
+
+it('reads demodata', async () => {
+    const inputArr = await readData('demoinput.txt');
+    
+    expect(processAnswerArray(inputArr)).toBe(11)
+})
+it('reads data', async () => {
+    const inputArr = await readData('puzzleinput.txt');
+    expect(processAnswerArray(inputArr)).toBe(6437)
+})
+
+it('reads demodata', async () => {
+    const inputArr = await readData('demoinput.txt');
+    
+    expect(processAnswerArray2(inputArr)).toBe(6)
+})
+
+it('reads demodata', async () => {
+    const inputArr = await readData('puzzleinput.txt');
+    
+    expect(processAnswerArray2(inputArr)).toBe(3229)
+})
---
+++ src/AdventOfCode/2020/06-CustomCustoms/06-CustomCustoms.ts
@@ -0,0 +1,28 @@
+
+export const processAnswerArray = (input: string[]): any => {
+    const groups = input.map(val => val === '' ? "\n" : val).join('').split('\n');
+    const countAnswers = groups.map(val => new Set(val.split("")).size)
+    return countAnswers.reduce((acc, val) => acc + val)
+}
+
+export const processAnswerArray2 = (input: string[]) => {
+    const groups:string[][] = [];
+    let index = 0;
+    input.forEach((val, i) => {
+        if (val !== '') {
+            groups[index] = [...(groups[index] ?? []), val]
+        } else {
+            index++;
+        }
+    })
+    const remapped = groups.map((val) => {
+        let mappedString = {}
+        val.map(str => {
+            str.split("").forEach(char => mappedString[char] = (mappedString[char] || 0) + 1 )
+        })
+        let count = Object.values(mappedString).filter(counter => counter === val.length).length
+        return count
+    })
+    return remapped.reduce((acc, val) => acc + val)
+  
+}
