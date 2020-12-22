362ab939428623fd9076bd9f0ed51deed5fb7bf0
Neal <info@nealbuerger.com>
2020-12-22 14:51:02 +0100

    day 21 - prework

---
+++ src/AdventOfCode/2020/21-AllergenAssesment/21-AllergenAssesment.test.ts
@@ -0,0 +1,23 @@
+import { readDemoData, readPuzzleData } from "../utils/readFiles"
+import { parseInputString, prepareList } from "./21-AllergenAssesment"
+
+it('processes an entry', async () => {
+    const data = await readDemoData(__dirname)
+    expect(parseInputString(data[0])).toEqual({
+        lang: [
+            "mxmxvkd",
+            "kfcds",
+            "sqjhc",
+            "nhms",
+        ], eng: [
+            "dairy",
+            "fish",
+        ]
+    })
+    expect(prepareList(data)).toEqual(5)
+})
+
+it('finds the solution', async () => {
+    const data = await readPuzzleData(__dirname)
+    expect(prepareList(data)).toEqual(120)
+})
