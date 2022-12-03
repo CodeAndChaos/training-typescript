fd83543eda0646a1644e330b9bf790341da73dbe
Neal <info@nealbuerger.com>
2022-12-03 19:44:53 +0100

    solution to advent of code

--- src/AdventOfCode/2022/01-CalorieCounting/01-CalorieCounting.test.ts
+++ src/AdventOfCode/2022/01-CalorieCounting/01-CalorieCounting.test.ts
@@ -1,7 +1,12 @@
 import { readPuzzleData } from "../../utils/readFiles";
-import { calorieCounting } from "./01-CalorieCounting";
+import { calorieCounting, calorieCountingPart2 } from "./01-CalorieCounting";
 
 it("counts the calories of the elves", async () => {
   const input = await readPuzzleData(__dirname);
   expect( calorieCounting(input)).toBe(68802)
+});
+
+it("counts the calories of the elves", async () => {
+  const input = await readPuzzleData(__dirname);
+  expect( calorieCountingPart2(input)).toBe(205370)
 });
--- src/AdventOfCode/2022/01-CalorieCounting/01-CalorieCounting.ts
+++ src/AdventOfCode/2022/01-CalorieCounting/01-CalorieCounting.ts
@@ -1,15 +1,24 @@
-export const calorieCounting = (input: string[]) => {
-  const newValues: number[] = [];
+const processElvesCarryInput = (input: string[]): number[] => {
+  const newValues: number[] = [0];
   let index = 0;
   input.forEach((val) => {
     if (val === "") {
       index++;
+      newValues[index] = 0;
     } else {
-      if (isNaN(newValues[index])) {
-        newValues[index] = 0;
-      }
       newValues[index] += parseInt(val, 10);
     }
   });
-  return Math.max(...newValues);
+  return newValues;
+};
+
+export const calorieCounting = (input: string[]) => {
+  const caloriesCount = processElvesCarryInput(input);
+  return Math.max(...caloriesCount);
+};
+
+export const calorieCountingPart2 = (input: string[]) => {
+  const caloriesCount = processElvesCarryInput(input);
+  caloriesCount.sort((a, b) => b - a);
+  return caloriesCount[0] + caloriesCount[1] + caloriesCount[2];
 };
